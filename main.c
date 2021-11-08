de566d55c424e9cac4f820fb46a1c8ca372756c9
Nikita <93082022+NikitaAnosov@users.noreply.github.com>
2021-11-08 11:40:11 +0200

    Update main.c

--- main.c
+++ main.c
@@ -4,32 +4,32 @@ int main()
 {
     int x, y; // x = first , y = last
     scanf("%d%d",&x,&y);
-        printf("The Armstrong numbers are: ");
+        printf("The Armstrong numbers are:");
     for (int i = x; i <= y; i++)
     {
         if (isArmstrong(i) == 1)
-            printf("%d ", i);
+            printf(" %d", i);
     }
     printf("\n");
     printf("The Palindromes are: ");
     for (int i = x; i <= y; i++)
     {
         if (isPalindrome(i) == 1)
-            printf("%d ", i);
+            printf(" %d", i);
     }
     printf("\n");
     printf("The Prime numbers are: ");
     for (int i = x; i <= y; i++)
     {
         if (isPrime(i) == 1)
-            printf("%d ", i);
+            printf(" %d", i);
     }
     printf("\n");
     printf("The Strong numbers are: ");
     for (int i = x; i <= y; i++)
     {
         if (isStrong(i) == 1)
-            printf("%d ", i);
+            printf(" %d", i);
     }
     return 0;
 }
